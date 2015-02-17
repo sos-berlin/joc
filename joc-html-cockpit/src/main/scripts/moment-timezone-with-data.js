e23db3700f0b0a90238362c24c4480dfdb0b45b1
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2015-02-17 12:21:14 +0100

    suppress error if timezone not supported

--- joc-html-cockpit/src/main/scripts/moment-timezone-with-data.js
+++ joc-html-cockpit/src/main/scripts/moment-timezone-with-data.js
@@ -359,7 +359,7 @@
 			if (this._z) {
 				moment.updateOffset(this);
 			} else {
-				logError("Moment Timezone has no data for " + name + ". See http://momentjs.com/timezone/docs/#/data-loading/.");
+				//logError("Moment Timezone has no data for " + name + ". See http://momentjs.com/timezone/docs/#/data-loading/.");
 			}
 			return this;
 		}
