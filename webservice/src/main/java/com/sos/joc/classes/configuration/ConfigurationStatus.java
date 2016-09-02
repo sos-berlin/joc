bcd6237427888db174d1f7554a119363526df76d
Santiago Aucejo <santiago.aucejo.petzoldt.git@sos-berlin.com>
2016-09-02 13:38:43 +0200

    JOC Cockpit: ConfigurationStatusSchema helper class

---
+++ webservice/src/main/java/com/sos/joc/classes/configuration/ConfigurationStatus.java
@@ -0,0 +1,14 @@
+package com.sos.joc.classes.configuration;
+
+import org.w3c.dom.Element;
+
+import com.sos.joc.model.common.ConfigurationStatusSchema;
+
+
+public class ConfigurationStatus {
+    
+    public ConfigurationStatusSchema getConfigurationStatus (Element element) {
+        // TODO
+        return null;
+    }
+}
