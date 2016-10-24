78d07aadea02b339036d3afb7b7675d787cc948c
Oliver Haufe <oliver.haufe@sos-berlin.com>
2016-10-25 00:39:20 +0200

    JOB - ./plan iteration1

--- webservice/src/main/java/com/sos/joc/plan/impl/PlanImpl.java
+++ webservice/src/main/java/com/sos/joc/plan/impl/PlanImpl.java
@@ -11,55 +11,19 @@ import org.slf4j.LoggerFactory;
 
 import com.sos.joc.Globals;
 import com.sos.joc.classes.JOCDefaultResponse;
-import com.sos.joc.classes.JOCJsonCommand;
 import com.sos.joc.classes.JOCResourceImpl;
-import com.sos.joc.classes.orders.OrdersVCallable;
 import com.sos.joc.exceptions.JocError;
 import com.sos.joc.exceptions.JocException;
-import com.sos.joc.model.order.OrderV200;
 import com.sos.joc.model.plan.Plan;
 import com.sos.joc.model.plan.PlanFilter;
 import com.sos.joc.model.plan.PlanItem;
-import com.sos.joc.model.order.OrderFilter;
-import com.sos.joc.order.resource.IOrderResource;
 import com.sos.joc.plan.resource.IPlanResource;
 
 @Path("plan")
 public class PlanImpl extends JOCResourceImpl implements IPlanResource {
     private static final Logger LOGGER = LoggerFactory.getLogger(PlanImpl.class);
-    private static final String API_CALL = "API-CALL: ./plan";
+    private static final String API_CALL = "./plan";
     
-    @Override
-    public JOCDefaultResponse postOrder(String accessToken, OrderFilter orderBody) throws Exception {
-        LOGGER.debug(API_CALL);
-        try {
-            JOCDefaultResponse jocDefaultResponse = init(orderBody.getJobschedulerId(), getPermissons(accessToken).getOrder().getView().isStatus());
-            if (jocDefaultResponse != null) {
-                return jocDefaultResponse;
-            }
-
-            JOCJsonCommand command = new JOCJsonCommand(dbItemInventoryInstance.getUrl());
-            command.addOrderCompactQuery(orderBody.getCompact());
-            OrderV200 entity = new OrderV200();
-
-            if (checkRequiredParameter("orderId", orderBody.getOrderId()) && checkRequiredParameter("jobChain", orderBody.getJobChain())) {
-                OrdersVCallable o = new OrdersVCallable(orderBody, command.getURI());
-                entity.setOrder(o.getOrder());
-                entity.setDeliveryDate(Date.from(Instant.now()));
-            }
-
-            return JOCDefaultResponse.responseStatus200(entity);
-        } catch (JocException e) {
-            e.addErrorMetaInfo(getMetaInfo(API_CALL, orderBody));
-            return JOCDefaultResponse.responseStatusJSError(e);
-        } catch (Exception e) {
-            JocError err = new JocError();
-            err.addMetaInfoOnTop(getMetaInfo(API_CALL, orderBody));
-            return JOCDefaultResponse.responseStatusJSError(e, err);
-        }
-
-    }
-
     @Override
     public JOCDefaultResponse postPlan(String accessToken, PlanFilter planFilter) throws Exception {
         LOGGER.debug(API_CALL);
