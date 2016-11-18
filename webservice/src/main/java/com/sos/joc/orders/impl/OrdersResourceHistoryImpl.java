3f413e5c0b24b5f91285273c13caffba05e673db
Uwe Risse <uwe.risse-git@sos-berlin.com>
2016-11-18 18:29:14 +0100

    JOC Cockpit Orders/history

--- webservice/src/main/java/com/sos/joc/orders/impl/OrdersResourceHistoryImpl.java
+++ webservice/src/main/java/com/sos/joc/orders/impl/OrdersResourceHistoryImpl.java
@@ -6,13 +6,18 @@ import java.util.List;
 
 import javax.ws.rs.Path;
 
+import com.sos.jitl.dailyplan.db.ReportTriggerDBLayer;
+import com.sos.jitl.reporting.db.DBItemReportTrigger;
+import com.sos.joc.Globals;
 import com.sos.joc.classes.JOCDefaultResponse;
 import com.sos.joc.classes.JOCResourceImpl;
+import com.sos.joc.classes.JobSchedulerDate;
 import com.sos.joc.exceptions.JocException;
 import com.sos.joc.model.order.OrderHistory;
 import com.sos.joc.model.order.OrderHistoryItem;
 import com.sos.joc.model.order.OrderHistoryState;
 import com.sos.joc.model.order.OrderHistoryStateText;
+import com.sos.joc.model.order.OrderPath;
 import com.sos.joc.model.order.OrdersFilter;
 import com.sos.joc.orders.resource.IOrdersResourceHistory;
 
@@ -25,52 +30,72 @@ public class OrdersResourceHistoryImpl extends JOCResourceImpl implements IOrder
     public JOCDefaultResponse postOrdersHistory(String accessToken, OrdersFilter ordersFilter) throws Exception {
         try {
             initLogging(API_CALL, ordersFilter);
-            JOCDefaultResponse jocDefaultResponse = init(accessToken, ordersFilter.getJobschedulerId(), getPermissons(accessToken).getOrder()
-                    .getView().isStatus());
+            JOCDefaultResponse jocDefaultResponse = init(accessToken, ordersFilter.getJobschedulerId(), getPermissons(accessToken).getOrder().getView().isStatus());
             if (jocDefaultResponse != null) {
                 return jocDefaultResponse;
             }
 
-            // Reading orders from the database tale inventory history tables
-            // filtered by
-            ordersFilter.getDateFrom(); // is ISO 8601 or something like 6h
-                                        // 1w 65y
-            ordersFilter.getDateTo(); // same as dateFrom
-            ordersFilter.getTimeZone();
+            Globals.beginTransaction();
             ordersFilter.getRegex();
-            ordersFilter.getOrders(); // list of wanted orders.
-
-            // TODO JOC Cockpit Webservice (Data coming from db Inventory
-            // History Tables)
 
             List<OrderHistoryItem> listHistory = new ArrayList<OrderHistoryItem>();
 
-            OrderHistoryItem history = new OrderHistoryItem();
-            history.setEndTime(new Date());
-            history.setHistoryId("-1");
-            history.setJobChain("myJobChain");
-            history.setNode("myNode");
-            history.setOrderId("myOrderId");
-            history.setPath("myPath");
-            history.setStartTime(new Date());
-            OrderHistoryState state = new OrderHistoryState();
-            state.setSeverity(0);
-            state.set_text(OrderHistoryStateText.INCOMPLETE);
-            history.setState(state);
-            listHistory.add(history);
+            ReportTriggerDBLayer reportTriggerDBLayer = new ReportTriggerDBLayer(Globals.sosHibernateConnection);
+            reportTriggerDBLayer.getFilter().setSchedulerId(ordersFilter.getJobschedulerId());
+            reportTriggerDBLayer.getFilter().setExecutedFrom(JobSchedulerDate.getDateFromDateFrom(ordersFilter.getDateFrom(), ordersFilter.getTimeZone()));
+            reportTriggerDBLayer.getFilter().setExecutedTo(JobSchedulerDate.getDateFromDateTo(ordersFilter.getDateTo(), ordersFilter.getTimeZone()));
+            reportTriggerDBLayer.getFilter().setTimeZone(ordersFilter.getTimeZone());
+
+            for (OrderPath orderPath : ordersFilter.getOrders()) {
+                reportTriggerDBLayer.getFilter().addOrderPath(orderPath.getJobChain(),orderPath.getOrderId());
+            }
+
+            List<DBItemReportTrigger> listOfReportTriggerDBItems = reportTriggerDBLayer.getSchedulerOrderHistoryListFromTo();
+
+            for (DBItemReportTrigger dbItemReportTrigger : listOfReportTriggerDBItems) {
+
+                OrderHistoryItem history = new OrderHistoryItem();
+                history.setEndTime(dbItemReportTrigger.getEndTime());
+                history.setHistoryId(String.valueOf(dbItemReportTrigger.getHistoryId()));
+                history.setJobChain(dbItemReportTrigger.getParentName());
+                history.setNode(dbItemReportTrigger.getState());
+                history.setOrderId(dbItemReportTrigger.getName());
+                history.setPath(dbItemReportTrigger.getFullOrderQualifier());
+                history.setStartTime(dbItemReportTrigger.getStartTime());
+                OrderHistoryState state = new OrderHistoryState();
+
+                if (dbItemReportTrigger.getStartTime() != null && dbItemReportTrigger.getEndTime() == null) {
+                    state.setSeverity(1);
+                    state.set_text(OrderHistoryStateText.INCOMPLETE);
+                } else {
+                    if (dbItemReportTrigger.haveError()) {
+                        state.setSeverity(2);
+                        state.set_text(OrderHistoryStateText.FAILED);
+                    } else {
+                        state.setSeverity(0);
+                        state.set_text(OrderHistoryStateText.SUCCESSFUL);
+                    }
+
+                }
+                history.setState(state);
+                history.setSurveyDate(dbItemReportTrigger.getCreated());
+                listHistory.add(history);
+
+            }
 
             OrderHistory entity = new OrderHistory();
             entity.setDeliveryDate(new Date());
             entity.setHistory(listHistory);
-
+ 
             return JOCDefaultResponse.responseStatus200(entity);
         } catch (JocException e) {
             e.addErrorMetaInfo(getJocError());
             return JOCDefaultResponse.responseStatusJSError(e);
         } catch (Exception e) {
             return JOCDefaultResponse.responseStatusJSError(e, getJocError());
+        } finally {
+            Globals.rollback();
         }
-
     }
 
 }
