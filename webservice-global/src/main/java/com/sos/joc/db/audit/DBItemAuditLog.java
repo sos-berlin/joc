e6a6be910becb3cfddf9b9f921c12794e8b4ea78
Santiago Aucejo <santiago.aucejo.petzoldt.git@sos-berlin.com>
2017-01-25 09:56:21 +0100

    Transient annotation removed for audit_log DBItem

--- webservice-global/src/main/java/com/sos/joc/db/audit/DBItemAuditLog.java
+++ webservice-global/src/main/java/com/sos/joc/db/audit/DBItemAuditLog.java
@@ -33,7 +33,6 @@ public class DBItemAuditLog extends DbItem implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = DBLayer.TABLE_INVENTORY_INSTANCES_SEQUENCE)
     @Column(name = "`ID`", nullable = false)
-    @Transient
     public Long getId() {
         return this.id;
     }
@@ -41,130 +40,109 @@ public class DBItemAuditLog extends DbItem implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = DBLayer.TABLE_INVENTORY_INSTANCES_SEQUENCE)
     @Column(name = "`ID`", nullable = false)
-    @Transient
     public void setId(Long val) {
         this.id = val;
     }
 
     /** Others */
     @Column(name = "`SCHEDULER_ID`", nullable = false)
-    @Transient
     public void setSchedulerId(String val) {
         this.schedulerId = val;
     }
 
     @Column(name = "`SCHEDULER_ID`", nullable = false)
-    @Transient
     public String getSchedulerId() {
         return this.schedulerId;
     }
     
     @Column(name = "`ACCOUNT`", nullable = false)
-    @Transient
     public String getAccount() {
         return account;
     }
     
     @Column(name = "`ACCOUNT`", nullable = false)
-    @Transient
     public void setAccount(String account) {
         this.account = account;
     }
     
     @Column(name = "`REQUEST`", nullable = false)
-    @Transient
     public String getRequest() {
         return request;
     }
     
     @Column(name = "`REQUEST`", nullable = false)
-    @Transient
     public void setRequest(String request) {
         this.request = request;
     }
     
     @Column(name = "`PARAMETERS`", nullable = true)
-    @Transient
     public String getParameters() {
         return parameters;
     }
     
     @Column(name = "`PARAMETERS`", nullable = true)
-    @Transient
     public void setParameters(String parameters) {
         this.parameters = parameters;
     }
     
     @Column(name = "`JOB`", nullable = true)
-    @Transient
     public String getJob() {
         return job;
     }
     
     @Column(name = "`JOB`", nullable = true)
-    @Transient
     public void setJob(String job) {
         this.job = job;
     }
     
     @Column(name = "`JOB_CHAIN`", nullable = true)
-    @Transient
     public String getJobChain() {
         return jobChain;
     }
     
     @Column(name = "`JOB_CHAIN`", nullable = true)
-    @Transient
     public void setJobChain(String jobChain) {
         this.jobChain = jobChain;
     }
     
     @Column(name = "`ORDER_ID`", nullable = true)
-    @Transient
     public String getOrderId() {
         return orderId;
     }
     
     @Column(name = "`ORDER_ID`", nullable = true)
-    @Transient
     public void setOrderId(String orderId) {
         this.orderId = orderId;
     }
     
     @Column(name = "`FOLDER`", nullable = true)
-    @Transient
     public String getFolder() {
         return folder;
     }
     
     @Column(name = "`FOLDER`", nullable = true)
-    @Transient
     public void setFolder(String folder) {
         this.folder = folder;
     }
     
     @Column(name = "`COMMENT`", nullable = true)
-    @Transient
     public String getComment() {
         return comment;
     }
     
     @Column(name = "`COMMENT`", nullable = true)
-    @Transient
     public void setComment(String comment) {
         this.comment = comment;
     }
     
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "`CREATED`", nullable = true)
-    @Transient
     public Date getCreated() {
         return created;
     }
     
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "`CREATED`", nullable = true)
-    @Transient
     public void setCreated(Date created) {
         this.created = created;
     }
