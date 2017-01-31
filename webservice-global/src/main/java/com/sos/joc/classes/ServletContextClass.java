077d8f9564d927fd8dc924d6cc9f4b8c16e5050b
Uwe Risse <uwe.risse-git@sos-berlin.com>
2017-01-31 12:33:17 +0100

    JOC Cockpit init and close db factory at startup/end webserver

---
+++ webservice-global/src/main/java/com/sos/joc/classes/ServletContextClass.java
@@ -0,0 +1,37 @@
+package com.sos.joc.classes;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import com.sos.JSHelper.Exceptions.JobSchedulerException;
+import com.sos.hibernate.classes.SOSHibernateFactory;
+import com.sos.joc.Globals;
+
+public class ServletContextClass implements ServletContextListener {
+
+    @Override
+    public void contextDestroyed(ServletContextEvent arg0) {
+        try {
+            Globals.getHibernateFactory();
+        } catch (Exception e) {
+            throw new JobSchedulerException(e);
+        }
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent arg0) {
+        if (Globals.sosHibernateFactory != null) {
+            Globals.sosHibernateFactory.close();
+        }
+
+        if (Globals.sosSchedulerHibernateFactories != null) {
+
+            for (SOSHibernateFactory factory : Globals.sosSchedulerHibernateFactories.values()) {
+                if (factory != null) {
+                    factory.close();
+                }
+            }
+        }
+    }
+
+}
