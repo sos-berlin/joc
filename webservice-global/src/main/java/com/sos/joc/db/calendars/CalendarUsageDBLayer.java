2b0329ef4ce10209e2ff8d796ab95207f63e799e
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2017-10-10 11:25:21 +0200

    JOC-267

--- webservice-global/src/main/java/com/sos/joc/db/calendars/CalendarUsageDBLayer.java
+++ webservice-global/src/main/java/com/sos/joc/db/calendars/CalendarUsageDBLayer.java
@@ -14,7 +14,6 @@ import com.sos.joc.exceptions.DBInvalidDataException;
 
 public class CalendarUsageDBLayer extends DBLayer {
     
-    private static final String CALENDAR_USAGE = CalendarUsage.class.getName();
     private Query<DBItemInventoryCalendarUsage> query;
 
     public CalendarUsageDBLayer(SOSHibernateSession connection) {
@@ -195,56 +194,6 @@ public class CalendarUsageDBLayer extends DBLayer {
             bindParameters(filter);
             return getSession().getResultList(query);
 
-        } catch (SOSHibernateInvalidSessionException ex) {
-            throw new DBConnectionRefusedException(ex);
-        } catch (Exception ex) {
-            throw new DBInvalidDataException(ex);
-        }
-    }
-    
-    public List<CalendarUsage> getCalendarUsages(Long calendarId)
-            throws DBInvalidDataException, DBConnectionRefusedException {
-        try {
-            StringBuilder sql = new StringBuilder();
-            sql.append("select new ").append(CALENDAR_USAGE);
-            sql.append(" (icu.instanceId, icu.path, icu.objectType, ii.schedulerId, ii.hostname, ii.port) from ");
-            sql.append(DBITEM_INVENTORY_CALENDAR_USAGE).append(" icu, ");
-            sql.append(DBITEM_INVENTORY_INSTANCES).append(" ii ");
-            sql.append("where icu.instanceId = ii.id ");
-            if (calendarId != null) {
-                sql.append(" and icu.calendarId = :calendarId");
-            }
-            Query<CalendarUsage> query = getSession().createQuery(sql.toString());
-            if (calendarId != null) {
-                query.setParameter("calendarId", calendarId);
-            }
-            return getSession().getResultList(query);
-        } catch (SOSHibernateInvalidSessionException ex) {
-            throw new DBConnectionRefusedException(ex);
-        } catch (Exception ex) {
-            throw new DBInvalidDataException(ex);
-        }
-    }
-    
-    public List<CalendarUsage> getCalendarUsages(String calendarPath)
-            throws DBInvalidDataException, DBConnectionRefusedException {
-        try {
-            StringBuilder sql = new StringBuilder();
-            sql.append("select new ").append(CALENDAR_USAGE);
-            sql.append(" (icu.instanceId, icu.path, icu.objectType, ii.schedulerId, ii.hostname, ii.port) from ");
-            sql.append(DBITEM_INVENTORY_CALENDAR_USAGE).append(" icu, ");
-            sql.append(DBITEM_CALENDARS).append(" c, ");
-            sql.append(DBITEM_INVENTORY_INSTANCES).append(" ii ");
-            sql.append("where icu.calendarId = c.id ");
-            sql.append("and icu.instanceId = ii.id ");
-            if (calendarPath != null) {
-                sql.append(" and c.name = :calendarPath");
-            }
-            Query<CalendarUsage> query = getSession().createQuery(sql.toString());
-            if (calendarPath != null) {
-                query.setParameter("calendarPath", calendarPath);
-            }
-            return getSession().getResultList(query);
         } catch (SOSHibernateInvalidSessionException ex) {
             throw new DBConnectionRefusedException(ex);
         } catch (Exception ex) {
