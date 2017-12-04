59c6631b09c846365cd42496ec7129cefa6bed76
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2017-12-04 17:05:13 +0100

    JOC-267

--- webserviceGenTest/src/main/java/com/sos/joc2/Main.java
+++ webserviceGenTest/src/main/java/com/sos/joc2/Main.java
@@ -8,6 +8,7 @@ import java.net.URI;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
  
 public class Main {
@@ -21,7 +22,7 @@ public class Main {
     public static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers
         // in com.sos.joc package
-       final ResourceConfig rc = new ResourceConfig().packages("com.sos.joc","com.sos.auth");
+       final ResourceConfig rc = new ResourceConfig().packages("com.sos.joc","com.sos.auth").register(MultiPartFeature.class);
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
