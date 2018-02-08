a4e934b21ae497aad357e00f403e9917dd4bcce3
Uwe Risse <uwe.risse-git@sos-berlin.com>
2018-02-08 10:09:01 +0100

    JOC-378

--- webservice/src/main/java/com/sos/joc/classes/security/SOSSecurityConfigurationMainEntry.java
+++ webservice/src/main/java/com/sos/joc/classes/security/SOSSecurityConfigurationMainEntry.java
@@ -4,16 +4,36 @@ import java.util.Map;
 
 public class SOSSecurityConfigurationMainEntry {
 
+    private String removeCharInQuotes(String s, String source, String target) {
+        boolean inQuote = false;
+        StringBuffer str = new StringBuffer(s);
+        for (int i = 0; i < s.length(); i++) {
+            String charAt = String.valueOf(s.charAt(i));
+            if ("\"".equals(charAt)) {
+                inQuote = !inQuote;
+            }
+            if (inQuote && source.equals(charAt)) {
+                str.setCharAt(i, target.charAt(0));
+            }
+
+        }
+        return str.toString();
+
+    }
+    
     public String getIniWriteString(Map.Entry<String, String> entry) {
         if (entry.getKey().contains(".groupRolesMap")) {
+            String value = removeCharInQuotes(entry.getValue(), ",", "°");
+
             String s = "  ";
-            String[] map = entry.getValue().split(",");
+            String[] map = value.split(",");
             if (map.length > 1) {
                 s = "\\" + "\n" + "  ";
             }
 
             for (int i = 0; i < map.length; i++) {
                 s = s + map[i].trim();
+                s = removeCharInQuotes(s, "°", ",");
                 if (i < map.length - 1) {
                     s = s + ", \\" + "\n" + "  ";
                 } else {
@@ -25,4 +45,5 @@ public class SOSSecurityConfigurationMainEntry {
             return entry.getValue();
         }
     }
+ 
 }
