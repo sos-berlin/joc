6dfbcecf348bb5519f329165bd162eca310a010e
Uwe Risse <uwe.risse-git@sos-berlin.com>
2018-09-27 10:09:35 +0200

    JOC-538

--- webservice/src/main/java/com/sos/joc/jobchains/impl/JobChainsResourceImpl.java
+++ webservice/src/main/java/com/sos/joc/jobchains/impl/JobChainsResourceImpl.java
@@ -23,60 +23,66 @@ import com.sos.joc.model.jobChain.JobsFilter;
 @Path("job_chains")
 public class JobChainsResourceImpl extends JOCResourceImpl implements IJobChainsResource {
 
-    private static final String API_CALL = "./job_chains";
+	private static final String API_CALL = "./job_chains";
 
-    @Override
-    public JOCDefaultResponse postJobChains(String xAccessToken, String accessToken, JobChainsFilter jobChainsFilter) throws Exception {
-        return postJobChains(getAccessToken(xAccessToken, accessToken), jobChainsFilter);
-    }
+	@Override
+	public JOCDefaultResponse postJobChains(String xAccessToken, String accessToken, JobChainsFilter jobChainsFilter)
+			throws Exception {
+		return postJobChains(getAccessToken(xAccessToken, accessToken), jobChainsFilter);
+	}
 
-    public JOCDefaultResponse postJobChains(String accessToken, JobChainsFilter jobChainsFilter) throws Exception {
-        try {
-            JOCDefaultResponse jocDefaultResponse = init(API_CALL, jobChainsFilter, accessToken, jobChainsFilter.getJobschedulerId(),
-                    getPermissonsJocCockpit(jobChainsFilter.getJobschedulerId(), accessToken).getJobChain().getView().isStatus());
-            if (jocDefaultResponse != null) {
-                return jocDefaultResponse;
-            }
+	public JOCDefaultResponse postJobChains(String accessToken, JobChainsFilter jobChainsFilter) throws Exception {
+		try {
+			JOCDefaultResponse jocDefaultResponse = init(API_CALL, jobChainsFilter, accessToken,
+					jobChainsFilter.getJobschedulerId(),
+					getPermissonsJocCockpit(jobChainsFilter.getJobschedulerId(), accessToken).getJobChain().getView()
+							.isStatus());
+			if (jocDefaultResponse != null) {
+				return jocDefaultResponse;
+			}
 
-            JobChainsV entity = new JobChainsV();
-           
-            jobChainsFilter.getJob().setRegex(SearchStringHelper.getRegexValue(jobChainsFilter.getJob().getRegex()));
-            jobChainsFilter.setRegex(SearchStringHelper.getRegexValue(jobChainsFilter.getRegex()));
-            
-            JOCXmlJobChainCommand jocXmlCommand = new JOCXmlJobChainCommand(this, accessToken);
-            List<JobChainPath> jobChains = jobChainsFilter.getJobChains();
-            boolean withFolderFilter = jobChainsFilter.getFolders() != null && !jobChainsFilter.getFolders().isEmpty();
-            List<Folder> folders = addPermittedFolder(jobChainsFilter.getFolders());
-            List<JobChainV> listOfJobChains = null;
+			JobChainsV entity = new JobChainsV();
 
-            if (jobChains != null && !jobChains.isEmpty()) {
-                List<JobChainPath> permittedJobChains = new ArrayList<JobChainPath>();
-                Set<Folder> permittedFolders = folderPermissions.getListOfFolders();
-                for (JobChainPath jobChain : jobChains) {
-                    if (jobChain != null && canAdd(jobChain.getJobChain(), permittedFolders)) {
-                        permittedJobChains.add(jobChain);
-                    }
-                }
-                if (!permittedJobChains.isEmpty()) {
-                    listOfJobChains = jocXmlCommand.getJobChainsFromShowJobChain(permittedJobChains, jobChainsFilter);
-                }
-            } else if (withFolderFilter && (folders == null || folders.isEmpty())) {
-                // no permission
-            } else if (folders != null && !folders.isEmpty()) {
-                listOfJobChains = jocXmlCommand.getJobChainsFromShowState(folders, jobChainsFilter);
-            } else {
-                listOfJobChains = jocXmlCommand.getJobChainsFromShowState(jobChainsFilter);
-            }
-            entity.setJobChains(listOfJobChains);
-            entity.setNestedJobChains(jocXmlCommand.getNestedJobChains());
-            entity.setDeliveryDate(new Date());
+			if (jobChainsFilter.getJob() != null) {
+				jobChainsFilter.getJob()
+						.setRegex(SearchStringHelper.getRegexValue(jobChainsFilter.getJob().getRegex()));
+				jobChainsFilter.setRegex(SearchStringHelper.getRegexValue(jobChainsFilter.getRegex()));
+			}
 
-            return JOCDefaultResponse.responseStatus200(entity);
-        } catch (JocException e) {
-            e.addErrorMetaInfo(getJocError());
-            return JOCDefaultResponse.responseStatusJSError(e);
-        } catch (Exception e) {
-            return JOCDefaultResponse.responseStatusJSError(e, getJocError());
-        }
-    }
+			JOCXmlJobChainCommand jocXmlCommand = new JOCXmlJobChainCommand(this, accessToken);
+			List<JobChainPath> jobChains = jobChainsFilter.getJobChains();
+			boolean withFolderFilter = jobChainsFilter.getFolders() != null && !jobChainsFilter.getFolders().isEmpty();
+			List<Folder> folders = addPermittedFolder(jobChainsFilter.getFolders());
+			List<JobChainV> listOfJobChains = null;
+
+			if (jobChains != null && !jobChains.isEmpty()) {
+				List<JobChainPath> permittedJobChains = new ArrayList<JobChainPath>();
+				Set<Folder> permittedFolders = folderPermissions.getListOfFolders();
+				for (JobChainPath jobChain : jobChains) {
+					if (jobChain != null && canAdd(jobChain.getJobChain(), permittedFolders)) {
+						permittedJobChains.add(jobChain);
+					}
+				}
+				if (!permittedJobChains.isEmpty()) {
+					listOfJobChains = jocXmlCommand.getJobChainsFromShowJobChain(permittedJobChains, jobChainsFilter);
+				}
+			} else if (withFolderFilter && (folders == null || folders.isEmpty())) {
+				// no permission
+			} else if (folders != null && !folders.isEmpty()) {
+				listOfJobChains = jocXmlCommand.getJobChainsFromShowState(folders, jobChainsFilter);
+			} else {
+				listOfJobChains = jocXmlCommand.getJobChainsFromShowState(jobChainsFilter);
+			}
+			entity.setJobChains(listOfJobChains);
+			entity.setNestedJobChains(jocXmlCommand.getNestedJobChains());
+			entity.setDeliveryDate(new Date());
+
+			return JOCDefaultResponse.responseStatus200(entity);
+		} catch (JocException e) {
+			e.addErrorMetaInfo(getJocError());
+			return JOCDefaultResponse.responseStatusJSError(e);
+		} catch (Exception e) {
+			return JOCDefaultResponse.responseStatusJSError(e, getJocError());
+		}
+	}
 }
