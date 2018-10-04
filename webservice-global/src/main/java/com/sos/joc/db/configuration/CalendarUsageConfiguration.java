f384261a83f54664dff9d1404146ad14c9575a02
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2018-10-04 16:31:11 +0200

    JOC-502

--- webservice-global/src/main/java/com/sos/joc/db/configuration/CalendarUsageConfiguration.java
+++ webservice-global/src/main/java/com/sos/joc/db/configuration/CalendarUsageConfiguration.java
@@ -1,21 +1,32 @@
 package com.sos.joc.db.configuration;
 
-import java.io.IOException;
-
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sos.joc.model.calendar.Calendar;
 import com.sos.joc.model.calendar.CalendarType;
 
 public class CalendarUsageConfiguration {
     
-    private Calendar c;
+    private Calendar c = null;
 
-    public CalendarUsageConfiguration(String basedOn, String type, String configuration) throws JsonParseException, JsonMappingException, IOException {
-        this.c = new ObjectMapper().readValue(configuration, Calendar.class);
-        this.c.setBasedOn(basedOn);
-        this.c.setType(CalendarType.fromValue(type));
+    public CalendarUsageConfiguration(String basedOn, String type, String configuration) {
+        if (configuration != null) {
+            try {
+                this.c = new ObjectMapper().readValue(configuration, Calendar.class);
+            } catch (Exception e) {
+            }
+        }
+        if (this.c != null) {
+            this.c.setBasedOn(basedOn);
+            CalendarType ctype = CalendarType.WORKING_DAYS;
+            if (type != null) {
+                try {
+                    ctype = CalendarType.fromValue(type);
+                } catch (IllegalArgumentException e) {
+                //
+                } 
+            }
+            this.c.setType(ctype);
+        }
     }
     
     public Calendar getCalendar() {
