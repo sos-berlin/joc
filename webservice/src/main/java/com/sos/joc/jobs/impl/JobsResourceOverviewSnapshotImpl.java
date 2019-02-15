8617adb1959e4f0ef45f942921a86e2bb37dac4d
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2019-02-15 15:08:01 +0100

    JOC-684

--- webservice/src/main/java/com/sos/joc/jobs/impl/JobsResourceOverviewSnapshotImpl.java
+++ webservice/src/main/java/com/sos/joc/jobs/impl/JobsResourceOverviewSnapshotImpl.java
@@ -2,16 +2,27 @@ package com.sos.joc.jobs.impl;
 
 import java.time.Instant;
 import java.util.Date;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonValue;
+import javax.json.JsonValue.ValueType;
 import javax.ws.rs.Path;
 
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 import com.sos.joc.classes.JOCDefaultResponse;
+import com.sos.joc.classes.JOCJsonCommand;
 import com.sos.joc.classes.JOCResourceImpl;
 import com.sos.joc.classes.JOCXmlCommand;
+import com.sos.joc.classes.JobSchedulerDate;
 import com.sos.joc.classes.WebserviceConstants;
 import com.sos.joc.classes.configuration.ConfigurationStatus;
 import com.sos.joc.exceptions.JobSchedulerConnectionResetException;
@@ -28,6 +39,30 @@ import com.sos.joc.model.job.JobsSummary;
 public class JobsResourceOverviewSnapshotImpl extends JOCResourceImpl implements IJobsResourceOverviewSnapshot {
 
     private static final String API_CALL = "./jobs/overview/snapshot";
+    public class SummaryStatistics {
+
+        private int count = 0;
+        private int countRunningTasks = 0;
+
+        public void accept(JsonValue jv) {
+            ++count;
+            countRunningTasks += ((JsonObject) jv).getInt("usedTaskCount", 0);
+        }
+        
+        public SummaryStatistics combine(SummaryStatistics other) {
+            this.count += other.count;
+            this.countRunningTasks += other.countRunningTasks;
+            return this;
+        }
+        
+        public final int getCount() {
+            return count;
+        }
+        
+        public final int getCountRunningTasks() {
+            return countRunningTasks;
+        }
+    }
 
     @Override
     public JOCDefaultResponse postJobsOverviewSnapshot(String accessToken, JobSchedulerId jobScheduler) throws Exception {
@@ -38,65 +73,126 @@ public class JobsResourceOverviewSnapshotImpl extends JOCResourceImpl implements
                 return jocDefaultResponse;
             }
 
-            JOCXmlCommand jocXmlCommand = new JOCXmlCommand(this);
-            String command = jocXmlCommand.getShowStateCommand("job", "", "", 0, 0);
-            jocXmlCommand.executePostWithThrowBadRequestAfterRetry(command, accessToken);
-            Integer countPending = 0;
-            Integer countRunning = 0;
-            Integer countStopped = 0;
-            Integer countWaitingForResource = 0;
-            Integer countRunningTasks = 0;
-            Integer countQueuedTasks = 0;
-            Integer curRunningTasks = 0;
-            Integer curQueuedTasks = 0;
-            NodeList jobStatistics = jocXmlCommand.getSosxml().selectNodeList("//job[not(@path='/scheduler_file_order_sink') and not(@path='/scheduler_service_forwarder') and @enabled='yes']");
-            Set<Folder> folders = folderPermissions.getListOfFolders();
-            for (int i = 0; i < jobStatistics.getLength(); i++) {
-                Element job = (Element) jobStatistics.item(i);
-                if (!jobIsInFolder(job.getAttribute("path"), folders)) {
-                    continue;
-                }
-                curRunningTasks = getNumOfRunningTasks(jocXmlCommand, job);
-                curQueuedTasks = getNumOfQueuedTasks(jocXmlCommand, job);
-                countRunningTasks = countRunningTasks + curRunningTasks;
-                countQueuedTasks = countQueuedTasks + curQueuedTasks;
-                JobStateText jobStateText = setState(jocXmlCommand, job, curRunningTasks, curQueuedTasks);
-                switch (jobStateText) {
-                case PENDING:
-                    countPending++;
-                    break;
-                case STOPPED:
-                    countStopped++;
-                    break;
-                case STOPPING:
-                case RUNNING:
-                    countRunning++;
-                    break;
-                case ERROR:
-                case NOT_IN_PERIOD:
-                case NOT_INITIALIZED:
-                case WAITING_FOR_AGENT:
-                case WAITING_FOR_LOCK:
-                case WAITING_FOR_PROCESS:
-                case WAITING_FOR_TASK:
-                    countWaitingForResource++;
-                    break;
-                case UNKNOWN:
-                case LOADED:
-                case INITIALIZED:
-                case DISABLED:
-                    break;
-                }
-            }
-            JobsSummary jobs = new JobsSummary();
-            jobs.setPending(countPending);
-            jobs.setRunning(countRunning);
-            jobs.setStopped(countStopped);
-            jobs.setWaitingForResource(countWaitingForResource);
-            jobs.setTasks(countRunningTasks);
-
             JobsSnapshot entity = new JobsSnapshot();
-            entity.setSurveyDate(jocXmlCommand.getSurveyDate());
+            JobsSummary jobs = new JobsSummary();
+
+            if (versionIsOlderThan("1.12.6")) {
+                JOCXmlCommand jocXmlCommand = new JOCXmlCommand(this);
+                String command = jocXmlCommand.getShowStateCommand("job folder", "folders", "", 0, 0);
+                jocXmlCommand.executePostWithThrowBadRequestAfterRetry(command, accessToken);
+                Integer countPending = 0;
+                Integer countRunning = 0;
+                Integer countStopped = 0;
+                Integer countWaitingForResource = 0;
+                Integer countRunningTasks = 0;
+                Integer countQueuedTasks = 0;
+                Integer curRunningTasks = 0;
+                Integer curQueuedTasks = 0;
+                NodeList jobStatistics = jocXmlCommand.getSosxml().selectNodeList(
+                        "//job[not(@path='/scheduler_file_order_sink') and not(@path='/scheduler_service_forwarder') and @enabled='yes']");
+                Set<Folder> folders = folderPermissions.getListOfFolders();
+                for (int i = 0; i < jobStatistics.getLength(); i++) {
+                    Element job = (Element) jobStatistics.item(i);
+                    if (!jobIsInFolder(job.getAttribute("path"), folders)) {
+                        continue;
+                    }
+                    curRunningTasks = getNumOfRunningTasks(jocXmlCommand, job);
+                    curQueuedTasks = getNumOfQueuedTasks(jocXmlCommand, job);
+                    countRunningTasks = countRunningTasks + curRunningTasks;
+                    countQueuedTasks = countQueuedTasks + curQueuedTasks;
+                    JobStateText jobStateText = setState(jocXmlCommand, job, curRunningTasks, curQueuedTasks);
+                    switch (jobStateText) {
+                    case PENDING:
+                        countPending++;
+                        break;
+                    case STOPPED:
+                        countStopped++;
+                        break;
+                    case STOPPING:
+                    case RUNNING:
+                        countRunning++;
+                        break;
+                    case ERROR:
+                    case NOT_IN_PERIOD:
+                    case NOT_INITIALIZED:
+                    case WAITING_FOR_AGENT:
+                    case WAITING_FOR_LOCK:
+                    case WAITING_FOR_PROCESS:
+                    case WAITING_FOR_TASK:
+                        countWaitingForResource++;
+                        break;
+                    case UNKNOWN:
+                    case LOADED:
+                    case INITIALIZED:
+                    case DISABLED:
+                        break;
+                    }
+                }
+                jobs.setPending(countPending);
+                jobs.setRunning(countRunning);
+                jobs.setStopped(countStopped);
+                jobs.setWaitingForResource(countWaitingForResource);
+                jobs.setTasks(countRunningTasks);
+
+                entity.setSurveyDate(jocXmlCommand.getSurveyDate());
+            } else {
+                JOCJsonCommand command = new JOCJsonCommand(this);
+                command.setUriBuilderForJobs();
+                command.addJobCompactQuery(true);
+                
+                JsonObject json = command.getJsonObjectFromPostWithRetry("{\"path\":\"/\"}", accessToken);
+                entity.setSurveyDate(JobSchedulerDate.getDateFromEventId(json.getJsonNumber("eventId").longValue()));
+                JsonArray elements = json.getJsonArray("elements");
+                
+                Map<String, SummaryStatistics> summary = elements.stream().filter(jobIsEnabled).collect(Collectors.groupingBy(p -> {
+                    JsonObject overview = ((JsonObject) p);
+                    JsonArray obstacles = overview.getJsonArray("obstacles");
+                    String stateText = overview.getString("state", "UNKNOWN").toUpperCase();
+                    switch (stateText) {
+                    case "STOPPED":
+                        return "stopped";
+                    case "ERROR":
+                    case "NOT_INITIALIZED":
+                        return "waitingForResource";
+                    case "UNKNOWN":
+                    case "LOADED":
+                    case "INITIALIZED":
+                        return "unknown";
+                    }
+                    Optional<JsonValue> obstacleWaitingTypeObject = obstacles.stream().filter(o -> ((JsonObject) o).getString("TYPE", "").startsWith("WaitingFor")).findFirst();
+                    if (obstacleWaitingTypeObject.isPresent()) {
+                        String obstacleWaitingType = ((JsonObject) obstacleWaitingTypeObject.get()).getString("TYPE");
+                        if (obstacleWaitingType.equals("WaitingForLocks")) {
+                            return "waitingForResource";
+                        } else if (obstacleWaitingType.equals("WaitingForProcessClass")) {
+                            return "waitingForResource";
+                        }
+                    } else if (overview.getBoolean("isOrderJob", false) && "PENDING".equals(stateText) && !overview.getBoolean("isInPeriod", true)) {
+                        return "waitingForResource";
+                    } else if (isWaitingForAgent(overview.getJsonObject("taskObstacles"))) {
+                        return "waitingForResource";
+                    }
+                    switch (stateText) {
+                    case "PENDING":
+                        return "pending";
+                    case "STOPPING":
+                    case "RUNNING":
+                        return "running";
+                    }
+                    return "unknown";
+                }, Collector.of(SummaryStatistics::new, SummaryStatistics::accept, SummaryStatistics::combine)));
+                
+                summary.putIfAbsent("pending", new SummaryStatistics());
+                summary.putIfAbsent("stopped", new SummaryStatistics());
+                summary.putIfAbsent("running", new SummaryStatistics());
+                summary.putIfAbsent("waitingForResource", new SummaryStatistics());
+                
+                jobs.setPending(summary.get("pending").getCount());
+                jobs.setRunning(summary.get("running").getCount());
+                jobs.setStopped(summary.get("stopped").getCount());
+                jobs.setWaitingForResource(summary.get("waitingForResource").getCount());
+                jobs.setTasks(summary.get("running").getCountRunningTasks() + summary.get("waitingForResource").getCountRunningTasks());
+            }
             entity.setJobs(jobs);
             entity.setDeliveryDate(Date.from(Instant.now()));
             return JOCDefaultResponse.responseStatus200(entity);
@@ -111,6 +207,44 @@ public class JobsResourceOverviewSnapshotImpl extends JOCResourceImpl implements
             return JOCDefaultResponse.responseStatusJSError(e, getJocError());
         }
 
+    }
+    
+    public static Predicate<JsonValue> jobIsEnabled = new Predicate<JsonValue>() {
+
+        @Override
+        public boolean test(JsonValue p) {
+            try {
+                JsonObject overview = ((JsonObject) p);
+                String path = overview.getString("path", "");
+                if (path.isEmpty() || path.equals("/scheduler_file_order_sink") || path.equals("/scheduler_service_forwarder") || !overview.getBoolean("enabled", true)) {
+                    return false;
+                } else {
+                    ConfigurationState confState = ConfigurationStatus.getConfigurationStatus(overview.getJsonArray("obstacles"));
+                    if (confState != null && confState.getSeverity() == 2) {
+                        return false;
+                    }
+                }
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        
+    };
+    
+    public static boolean isWaitingForAgent(JsonObject taskObstacles) {
+        if (taskObstacles != null) {
+            for (JsonValue j : taskObstacles.values()) {
+                if (j.getValueType() == ValueType.ARRAY) {
+                    if (((JsonArray) j).stream().filter(p -> ((JsonObject) p).getString("TYPE", "").equals("WaitingForAgent")).findFirst().isPresent()) {
+                        return true;
+                    }
+                } else {
+                    continue;
+                }
+            }
+        }
+        return false;
     }
 
     private JobStateText setState(JOCXmlCommand jocXmlCommand, Element job, Integer runningTasks, Integer queuedTasks) throws Exception {
--- webservice/src/main/java/com/sos/joc/orders/impl/OrdersResourceOverviewSnapshotImpl.java
+++ webservice/src/main/java/com/sos/joc/orders/impl/OrdersResourceOverviewSnapshotImpl.java
@@ -2,17 +2,30 @@ package com.sos.joc.orders.impl;
 
 import java.time.Instant;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import javax.ws.rs.Path;
 
 import com.sos.joc.classes.JOCDefaultResponse;
 import com.sos.joc.classes.JOCJsonCommand;
 import com.sos.joc.classes.JOCResourceImpl;
-import com.sos.joc.classes.orders.Orders;
+import com.sos.joc.classes.JobSchedulerDate;
+import com.sos.joc.classes.orders.OrdersSnapshotCallable;
+import com.sos.joc.classes.orders.OrdersSnapshotEvent;
 import com.sos.joc.exceptions.JobSchedulerConnectionResetException;
+import com.sos.joc.exceptions.JobSchedulerObjectNotExistException;
 import com.sos.joc.exceptions.JocException;
+import com.sos.joc.exceptions.JocMissingRequiredParameterException;
 import com.sos.joc.model.common.Folder;
+import com.sos.joc.model.jobChain.JobChainPath;
 import com.sos.joc.model.jobChain.JobChainsFilter;
 import com.sos.joc.model.order.OrdersSnapshot;
 import com.sos.joc.model.order.OrdersSummary;
@@ -49,7 +62,7 @@ public class OrdersResourceOverviewSnapshotImpl extends JOCResourceImpl implemen
                 entity.setDeliveryDate(entity.getSurveyDate());
             } else {
                 jobChainsFilter.setFolders(folders);
-                entity = Orders.getSnapshot(new JOCJsonCommand(this), accessToken, jobChainsFilter);
+                entity = getSnapshot(new JOCJsonCommand(this), accessToken, jobChainsFilter);
             }
 
             return JOCDefaultResponse.responseStatus200(entity);
@@ -64,5 +77,135 @@ public class OrdersResourceOverviewSnapshotImpl extends JOCResourceImpl implemen
             return JOCDefaultResponse.responseStatusJSError(e, getJocError());
         }
 
+    }
+	
+	private OrdersSnapshot getSnapshot(JOCJsonCommand command, String accessToken, JobChainsFilter jobChainsFilter) throws Exception {
+        command.setUriBuilderForOrders();
+        command.addOrderStatisticsQuery();
+        //command.createHttpClient();
+
+        Set<String> jobChains = getJobChainsWithoutDuplicates(jobChainsFilter.getJobChains());
+        Set<String> folders = getFoldersWithoutDuplicatesAndSubfolders(jobChainsFilter.getFolders());
+        Set<OrdersSnapshotCallable> tasks = new HashSet<OrdersSnapshotCallable>();
+
+        OrdersSnapshot entity = new OrdersSnapshot();
+
+        if (jobChains.size() > 0) {
+            for (String jobChain : jobChains) {
+                tasks.add(new OrdersSnapshotCallable(jobChain, new JOCJsonCommand(command), accessToken));
+            }
+        } else if (folders.size() > 0) {
+            for (String folder : folders) {
+                tasks.add(new OrdersSnapshotCallable(folder, new JOCJsonCommand(command), accessToken));
+            }
+        } else {
+            tasks.add(new OrdersSnapshotCallable("/", command, accessToken));
+        }
+
+        if (!tasks.isEmpty()) {
+            if (tasks.size() == 1) {
+                OrdersSnapshotEvent oe = tasks.iterator().next().call();
+                entity.setSurveyDate(JobSchedulerDate.getDateFromEventId(oe.getEventId()));
+                entity.setOrders(oe);
+            } else {
+                Long eventId = 0L;
+                OrdersSummary summary = new OrdersSummary();
+                summary.setBlacklist(0);
+                summary.setPending(0);
+                summary.setRunning(0);
+                summary.setSetback(0);
+                summary.setSuspended(0);
+                summary.setWaitingForResource(0);
+
+                ExecutorService executorService = Executors.newFixedThreadPool(Math.min(10, tasks.size()));
+                try {
+                    for (Future<OrdersSnapshotEvent> result : executorService.invokeAll(tasks)) {
+                        try {
+                            OrdersSnapshotEvent o = result.get();
+                            summary.setBlacklist(summary.getBlacklist() + o.getBlacklist());
+                            summary.setPending(summary.getPending() + o.getPending());
+                            summary.setRunning(summary.getRunning() + o.getRunning());
+                            summary.setSetback(summary.getSetback() + o.getSetback());
+                            summary.setSuspended(summary.getSuspended() + o.getSuspended());
+                            summary.setWaitingForResource(summary.getWaitingForResource() + o.getWaitingForResource());
+                            long event = o.getEventId();
+                            if (event > eventId) {
+                                eventId = event;
+                            }
+                        } catch (ExecutionException e) {
+                            if (e.getCause() instanceof JobSchedulerObjectNotExistException) {
+                                //
+                            } else if (e.getCause() instanceof JocException) {
+                                throw (JocException) e.getCause();
+                            } else {
+                                throw (Exception) e.getCause();
+                            }
+                        }
+                    }
+                } finally {
+                    executorService.shutdown();
+                }
+
+                entity.setSurveyDate(JobSchedulerDate.getDateFromEventId(eventId));
+                entity.setOrders(summary);
+            }
+        }
+
+        entity.setDeliveryDate(Date.from(Instant.now()));
+        return entity;
+    }
+
+    private String getPostBody(Folder folder) throws JocMissingRequiredParameterException {
+        String path = folder.getFolder();
+        if (path == null) {
+            throw new JocMissingRequiredParameterException("folder");
+        } else {
+            path = ("/" + path.trim() + "/").replaceAll("//+", "/");
+            if (!folder.getRecursive()) {
+                path += "*";
+            }
+        }
+        return path;
+    }
+
+    private Set<String> getFoldersWithoutDuplicatesAndSubfolders(List<Folder> folders) throws JocMissingRequiredParameterException {
+        Set<String> set = new HashSet<String>();
+        if (folders == null || folders.size() == 0) {
+            return set;
+        }
+        SortedSet<String> sortedSet = new TreeSet<String>();
+        for (Folder folder : folders) {
+            sortedSet.add(getPostBody(folder));
+        }
+        String[] strA = new String[sortedSet.size()];
+        int index = 0;
+        for (String str : sortedSet) {
+            if (index > 0) {
+                if (!str.contains(strA[index - 1])) {
+                    strA[index] = str;
+                    index += 1;
+                    set.add(str);
+                }
+            } else {
+                strA[index] = str;
+                index += 1;
+                set.add(str);
+            }
+        }
+        return set;
+    }
+
+    private Set<String> getJobChainsWithoutDuplicates(List<JobChainPath> jobChains) throws JocMissingRequiredParameterException {
+        Set<String> set = new HashSet<String>();
+        if (jobChains == null || jobChains.size() == 0) {
+            return set;
+        }
+        for (JobChainPath jobChain : jobChains) {
+            if (jobChain.getJobChain() == null || jobChain.getJobChain().isEmpty()) {
+                throw new JocMissingRequiredParameterException("jobChain");
+            }
+            set.add(("/" + jobChain.getJobChain()).replaceAll("//+", "/").replaceFirst("/$", ""));
+        }
+        return set;
     }
 }
