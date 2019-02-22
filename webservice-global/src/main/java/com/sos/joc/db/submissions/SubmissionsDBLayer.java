99a47f29907fcea730dc4299d0a12ef9aa28ee0b
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2019-02-22 14:17:14 +0100

    JOC-682

---
+++ webservice-global/src/main/java/com/sos/joc/db/submissions/SubmissionsDBLayer.java
@@ -0,0 +1,109 @@
+package com.sos.joc.db.submissions;
+
+import java.sql.Date;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.query.Query;
+
+import com.sos.hibernate.classes.SOSHibernateSession;
+import com.sos.hibernate.exceptions.SOSHibernateInvalidSessionException;
+import com.sos.jitl.reporting.db.DBItemSubmission;
+import com.sos.jitl.reporting.db.DBItemSubmittedObject;
+import com.sos.jitl.reporting.db.DBLayer;
+import com.sos.joc.exceptions.DBConnectionRefusedException;
+import com.sos.joc.exceptions.DBInvalidDataException;
+
+public class SubmissionsDBLayer extends DBLayer {
+
+    public SubmissionsDBLayer(SOSHibernateSession session) {
+        super(session);
+    }
+
+    public List<DBItemSubmission> getSubmissionsBySubmissionId(Long submissionId) throws DBConnectionRefusedException, DBInvalidDataException {
+        try {
+            StringBuilder sql = new StringBuilder();
+            sql.append("from ").append(DBITEM_SUBMISSIONS);
+            sql.append(" where submissionId = :submissionId");
+            Query<DBItemSubmission> query = getSession().createQuery(sql.toString());
+            query.setParameter("submissionId", submissionId);
+            List<DBItemSubmission> result = getSession().getResultList(query);
+            if (result == null) {
+                result = new ArrayList<DBItemSubmission>();
+            }
+            return result;
+        } catch (SOSHibernateInvalidSessionException ex) {
+            throw new DBConnectionRefusedException(ex);
+        } catch (Exception ex) {
+            throw new DBInvalidDataException(ex);
+        }
+    }
+
+    public DBItemSubmission saveSubmission(DBItemSubmission dbItem) throws DBConnectionRefusedException, DBInvalidDataException {
+        try {
+            getSession().save(dbItem);
+            return dbItem;
+        } catch (SOSHibernateInvalidSessionException ex) {
+            throw new DBConnectionRefusedException(ex);
+        } catch (Exception ex) {
+            throw new DBInvalidDataException(ex);
+        }
+    }
+
+    public void deleteSubmission(DBItemSubmission dbItem) throws DBConnectionRefusedException, DBInvalidDataException {
+        try {
+            getSession().delete(dbItem);
+        } catch (SOSHibernateInvalidSessionException ex) {
+            throw new DBConnectionRefusedException(ex);
+        } catch (Exception ex) {
+            throw new DBInvalidDataException(ex);
+        }
+    }
+
+    public DBItemSubmittedObject getSubmittedObject(String jobschedulerId, String path) throws DBConnectionRefusedException,
+            DBInvalidDataException {
+        try {
+            StringBuilder sql = new StringBuilder();
+            sql.append("from ").append(DBITEM_SUBMITTED_OBJECTS);
+            sql.append(" where jobschedulerId = :jobschedulerId");
+            sql.append(" and path = :path");
+            Query<DBItemSubmittedObject> query = getSession().createQuery(sql.toString());
+            query.setParameter("jobschedulerId", jobschedulerId);
+            query.setParameter("path", path);
+            return getSession().getSingleResult(query);
+        } catch (SOSHibernateInvalidSessionException ex) {
+            throw new DBConnectionRefusedException(ex);
+        } catch (Exception ex) {
+            throw new DBInvalidDataException(ex);
+        }
+    }
+
+    public DBItemSubmittedObject saveOrUpdateSubmittedObject(DBItemSubmittedObject dbItem) throws DBConnectionRefusedException,
+            DBInvalidDataException {
+        try {
+            dbItem.setModified(Date.from(Instant.now()));
+            if (dbItem.getId() != null) {
+                getSession().update(dbItem);
+            } else {
+                getSession().save(dbItem);
+            }
+            return dbItem;
+        } catch (SOSHibernateInvalidSessionException ex) {
+            throw new DBConnectionRefusedException(ex);
+        } catch (Exception ex) {
+            throw new DBInvalidDataException(ex);
+        }
+    }
+
+    public void deleteSubmittedObject(DBItemSubmittedObject dbItem) throws DBConnectionRefusedException, DBInvalidDataException {
+        try {
+            getSession().delete(dbItem);
+        } catch (SOSHibernateInvalidSessionException ex) {
+            throw new DBConnectionRefusedException(ex);
+        } catch (Exception ex) {
+            throw new DBInvalidDataException(ex);
+        }
+    }
+
+}
--- webservice/src/main/java/com/sos/joc/agent/impl/AgentConfigurationResourceImpl.java
+++ webservice/src/main/java/com/sos/joc/agent/impl/AgentConfigurationResourceImpl.java
@@ -2,6 +2,7 @@ package com.sos.joc.agent.impl;
 
 import java.nio.file.Paths;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -10,9 +11,13 @@ import javax.ws.rs.Path;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.sos.hibernate.classes.SOSHibernateSession;
 import com.sos.jitl.reporting.db.DBItemInventoryInstance;
+import com.sos.jitl.reporting.db.DBItemSubmission;
+import com.sos.jitl.reporting.db.DBItemSubmittedObject;
 import com.sos.joc.Globals;
 import com.sos.joc.agent.resource.IAgentConfigurationResource;
 import com.sos.joc.classes.JOCDefaultResponse;
@@ -22,6 +27,7 @@ import com.sos.joc.classes.JobSchedulerDate;
 import com.sos.joc.classes.XMLBuilder;
 import com.sos.joc.classes.configuration.ConfigurationUtils;
 import com.sos.joc.db.inventory.instances.InventoryInstancesDBLayer;
+import com.sos.joc.db.submissions.SubmissionsDBLayer;
 import com.sos.joc.exceptions.DBConnectionRefusedException;
 import com.sos.joc.exceptions.DBInvalidDataException;
 import com.sos.joc.exceptions.DBOpenSessionException;
@@ -37,6 +43,8 @@ import com.sos.joc.model.agent.ModifyAgent;
 public class AgentConfigurationResourceImpl extends JOCResourceImpl implements IAgentConfigurationResource {
 
     private static final String API_CALL = "./agent/";
+    private static final String FILE_EXTENSION = ".process_class.xml";
+    private XmlMapper xmlMapper = new XmlMapper();
 
     @Override
     public JOCDefaultResponse readAgentConfiguration(String accessToken, AgentConfigurationFilter agentFilter) throws Exception {
@@ -47,11 +55,11 @@ public class AgentConfigurationResourceImpl extends JOCResourceImpl implements I
                 return jocDefaultResponse;
             }
             checkRequiredParameter("path", agentFilter.getPath());
-            
+
             AgentConfiguration200 entity = new AgentConfiguration200();
             entity.setJobschedulerId(agentFilter.getJobschedulerId());
             entity.setPath(normalizePath(agentFilter.getPath()));
-            
+
             String xPath = String.format("/spooler/answer//process_classes/process_class[@path='%s']/source/process_class", entity.getPath());
             JOCXmlCommand jocXmlCommand = new JOCXmlCommand(this);
             String command = jocXmlCommand.getShowStateCommand("folder process_class", "folders no_subfolders source", getParent(entity.getPath()));
@@ -61,7 +69,7 @@ public class AgentConfigurationResourceImpl extends JOCResourceImpl implements I
             if (fileBased != null) {
                 entity.setConfigurationDate(JobSchedulerDate.getDateFromISO8601String(fileBased.getAttribute("last_write_time")));
             }
-            entity.setConfiguration(new XmlMapper().readValue(ConfigurationUtils.getSourceXmlBytes(sourceNode), AgentConfiguration.class));
+            entity.setConfiguration(xmlMapper.readValue(ConfigurationUtils.getSourceXmlBytes(sourceNode), AgentConfiguration.class));
             entity.setDeliveryDate(Date.from(Instant.now()));
             return JOCDefaultResponse.responseStatus200(entity);
         } catch (JocException e) {
@@ -88,21 +96,21 @@ public class AgentConfigurationResourceImpl extends JOCResourceImpl implements I
             if (agent.getRemoteSchedulers() != null && agent.getRemoteSchedulers().isEmpty()) {
                 agent.setRemoteSchedulers(null);
             }
-            
+
             java.nio.file.Path path = Paths.get(configuration.getPath());
             agent.setName(path.getFileName().toString());
-            
+
             ModifyAgent modifyAgent = new ModifyAgent();
             modifyAgent.setFolder(path.getParent().toString().replace('\\', '/'));
             modifyAgent.setProcessClass(agent);
-            
-            String command = new XmlMapper().writeValueAsString(modifyAgent).replaceAll("<remote_schedulers ", "<remote_scheduler ");
-            
+
+            String command = xmlMapper.writeValueAsString(modifyAgent).replaceAll("<remote_schedulers ", "<remote_scheduler ");
+
             JOCXmlCommand jocXmlCommand = new JOCXmlCommand(this);
             jocXmlCommand.executePostWithThrowBadRequestAfterRetry(command, accessToken);
 
-            updateOtherClusterMembers("save", command, configuration.getJobschedulerId());
-            
+            updateAtOtherClusterMembers("save", command, configuration, agent);
+
             return null;
         } catch (JocException e) {
             e.addErrorMetaInfo(getJocError());
@@ -111,7 +119,7 @@ public class AgentConfigurationResourceImpl extends JOCResourceImpl implements I
             return JOCDefaultResponse.responseStatusJSError(e, getJocError());
         }
     }
-    
+
     @Override
     public JOCDefaultResponse deleteAgentConfiguration(String accessToken, AgentConfigurationFilter agentFilter) throws Exception {
         try {
@@ -124,9 +132,9 @@ public class AgentConfigurationResourceImpl extends JOCResourceImpl implements I
             String command = XMLBuilder.create("process_class.remove").addAttribute("process_class", agentFilter.getPath()).asXML();
             JOCXmlCommand jocXmlCommand = new JOCXmlCommand(this);
             jocXmlCommand.executePostWithThrowBadRequestAfterRetry(command, accessToken);
-            
-            updateOtherClusterMembers("delete", command, agentFilter.getJobschedulerId());
-            
+
+            deleteAtOtherClusterMembers("delete", command, agentFilter);
+
             return JOCDefaultResponse.responseStatusJSOk(jocXmlCommand.getSurveyDate());
         } catch (JocException e) {
             e.addErrorMetaInfo(getJocError());
@@ -135,35 +143,88 @@ public class AgentConfigurationResourceImpl extends JOCResourceImpl implements I
             return JOCDefaultResponse.responseStatusJSError(e, getJocError());
         }
     }
-    
-    private void updateOtherClusterMembers(String identifier, String command, String jobSchedulerId) throws JocConfigurationException,
-            DBOpenSessionException, DBInvalidDataException, DBConnectionRefusedException {
+
+    private void deleteAtOtherClusterMembers(String identifier, String command, AgentConfigurationFilter agentFilter)
+            throws JocConfigurationException, DBOpenSessionException, DBInvalidDataException, DBConnectionRefusedException {
         if (!"standalone".equals(dbItemInventoryInstance.getClusterType())) {
-            //ask db for other cluster members
-            SOSHibernateSession connection = null;
-            JOCXmlCommand jocXmlCommand = null;
-            try {
-                connection = Globals.createSosHibernateStatelessConnection(API_CALL + identifier);
-                InventoryInstancesDBLayer dbLayer = new InventoryInstancesDBLayer(connection);
-                List<DBItemInventoryInstance> clusterMembers = dbLayer.getInventoryInstancesBySchedulerId(jobSchedulerId);
-                if (clusterMembers != null) {
-                    for (DBItemInventoryInstance clusterMember : clusterMembers) {
-                        if (dbItemInventoryInstance.getId() == clusterMember.getId()) {
-                            // current JobScheduler is already updated (see above)
-                            continue;
+            DBItemSubmittedObject dbItem = new DBItemSubmittedObject();
+            dbItem.setSchedulerId(agentFilter.getJobschedulerId());
+            dbItem.setPath(agentFilter.getPath() + FILE_EXTENSION);
+            dbItem.setToDelete(true);
+            updateOtherClusterMembers(identifier, command, dbItem);
+        }
+    }
+
+    private void updateAtOtherClusterMembers(String identifier, String command, AgentConfiguration200 configuration, AgentConfiguration agent)
+            throws JocConfigurationException, DBOpenSessionException, DBInvalidDataException, DBConnectionRefusedException, JsonProcessingException {
+        if (!"standalone".equals(dbItemInventoryInstance.getClusterType())) {
+            DBItemSubmittedObject dbItem = new DBItemSubmittedObject();
+            dbItem.setSchedulerId(configuration.getJobschedulerId());
+            dbItem.setPath(configuration.getPath() + FILE_EXTENSION);
+            dbItem.setToDelete(false);
+            agent.setName(null);
+            xmlMapper.configure(SerializationFeature.INDENT_OUTPUT, Boolean.TRUE);
+            String agentXml = xmlMapper.writeValueAsString(agent).replaceAll("<remote_schedulers ", "<remote_scheduler ");
+            //dbItem.setContent("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n\n" + agentXml);
+            dbItem.setContent(agentXml);
+            updateOtherClusterMembers(identifier, command, dbItem);
+        }
+    }
+
+    private void updateOtherClusterMembers(String identifier, String command, DBItemSubmittedObject dbItem) throws JocConfigurationException,
+            DBOpenSessionException, DBInvalidDataException, DBConnectionRefusedException {
+        // ask db for other cluster members
+        SOSHibernateSession connection = null;
+        JOCXmlCommand jocXmlCommand = null;
+        boolean agentIsUpdated = false;
+        try {
+            connection = Globals.createSosHibernateStatelessConnection(API_CALL + identifier);
+            InventoryInstancesDBLayer dbInstancesLayer = new InventoryInstancesDBLayer(connection);
+            List<DBItemInventoryInstance> clusterMembers = dbInstancesLayer.getInventoryInstancesBySchedulerId(dbItem.getSchedulerId());
+            SubmissionsDBLayer dbSubmissionsLayer = new SubmissionsDBLayer(connection);
+            DBItemSubmittedObject oldDbItem = dbSubmissionsLayer.getSubmittedObject(dbItem.getSchedulerId(), dbItem.getPath());
+            List<DBItemSubmission> submissions = new ArrayList<DBItemSubmission>();
+            if (oldDbItem != null) {
+                dbItem.setId(oldDbItem.getId());
+                submissions = dbSubmissionsLayer.getSubmissionsBySubmissionId(oldDbItem.getId());
+            }
+            if (clusterMembers != null) {
+                for (DBItemInventoryInstance clusterMember : clusterMembers) {
+                    if (dbItemInventoryInstance.getId() == clusterMember.getId()) {
+                        // current JobScheduler is already updated (see above)
+                        continue;
+                    }
+                    try {
+                        clusterMember = Globals.jocConfigurationProperties.setUrlMapping(clusterMember);
+                        jocXmlCommand = new JOCXmlCommand(clusterMember);
+                        jocXmlCommand.executePostWithThrowBadRequestAfterRetry(command, getAccessToken());
+                    } catch (Exception e) {
+                        // if error then store agent conf in db for inventory plugin
+                        if (agentIsUpdated) {
+                            dbItem = dbSubmissionsLayer.saveOrUpdateSubmittedObject(dbItem);
+                            agentIsUpdated = true;
                         }
-                        try {
-                            clusterMember = Globals.jocConfigurationProperties.setUrlMapping(clusterMember);
-                            jocXmlCommand = new JOCXmlCommand(clusterMember);
-                            jocXmlCommand.executePostWithThrowBadRequestAfterRetry(command, getAccessToken());
-                        } catch (Exception e) {
-                            //if error then store agent conf in db for inventory plugin
+                        DBItemSubmission dbItemSubmission = new DBItemSubmission();
+                        dbItemSubmission.setInstanceId(clusterMember.getId());
+                        dbItemSubmission.setSubmissionId(dbItem.getId());
+                        if (!submissions.remove(dbItemSubmission)) {
+                            dbItemSubmission = dbSubmissionsLayer.saveSubmission(dbItemSubmission);
                         }
                     }
                 }
-            } finally {
-                Globals.disconnect(connection);
             }
+            if (agentIsUpdated) {
+                for (DBItemSubmission submission : submissions) {
+                    dbSubmissionsLayer.deleteSubmission(submission);
+                }
+            } else if (oldDbItem != null) {
+                dbSubmissionsLayer.deleteSubmittedObject(oldDbItem);
+                for (DBItemSubmission submission : submissions) {
+                    dbSubmissionsLayer.deleteSubmission(submission);
+                }
+            }
+        } finally {
+            Globals.disconnect(connection);
         }
     }
 
