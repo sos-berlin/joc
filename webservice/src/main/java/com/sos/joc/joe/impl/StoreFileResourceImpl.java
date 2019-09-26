dc589fe1ed91914aa0f446441984338f776dcb44
Uwe Risse <uwe.risse-git@sos-berlin.com>
2019-09-26 13:18:24 +0200

    JOC-730

--- webservice/src/main/java/com/sos/joc/joe/impl/StoreFileResourceImpl.java
+++ webservice/src/main/java/com/sos/joc/joe/impl/StoreFileResourceImpl.java
@@ -1,27 +1,19 @@
 package com.sos.joc.joe.impl;
 
-import java.sql.Date;
-import java.time.Instant;
-
 import javax.ws.rs.Path;
 
-import com.sos.auth.rest.permission.model.SOSPermissionJocCockpit;
 import com.sos.hibernate.classes.SOSHibernateSession;
 import com.sos.jitl.joe.DBItemJoeObject;
-import com.sos.jitl.reporting.helper.EConfigFileExtensions;
 import com.sos.joc.Globals;
 import com.sos.joc.classes.JOCDefaultResponse;
-import com.sos.joc.classes.JOCHotFolder;
 import com.sos.joc.classes.JOCResourceImpl;
 import com.sos.joc.db.joe.DBLayerJoeObjects;
 import com.sos.joc.db.joe.FilterJoeObjects;
 import com.sos.joc.exceptions.JocException;
 import com.sos.joc.joe.common.Helper;
-import com.sos.joc.joe.resource.IReadFileResource;
 import com.sos.joc.joe.resource.IStoreFileResource;
 import com.sos.joc.model.common.JobSchedulerObjectType;
 import com.sos.joc.model.joe.common.JSObjectEdit;
-import com.sos.joc.model.joe.processclass.ProcessClass;
 
 @Path("joe")
 public class StoreFileResourceImpl extends JOCResourceImpl implements IStoreFileResource {
@@ -29,7 +21,8 @@ public class StoreFileResourceImpl extends JOCResourceImpl implements IStoreFile
     private static final String API_CALL = "./joe/read/file";
 
     @Override
-    public JOCDefaultResponse storeFile(final String accessToken, final byte[] jsObj) {
+    //public JOCDefaultResponse storeFile(final String accessToken, final JSObjectEdit body) {
+       public JOCDefaultResponse storeFile(final String accessToken, final byte[] jsObj) {
         SOSHibernateSession sosHibernateSession = null;
         try {
             JSObjectEdit body = Globals.objectMapper.readValue(jsObj, JSObjectEdit.class);
@@ -42,60 +35,45 @@ public class StoreFileResourceImpl extends JOCResourceImpl implements IStoreFile
             checkRequiredParameter("path", body.getPath());
             String path = normalizePath(body.getPath() + "/");
 
-            if (!this.folderPermissions.isPermittedForFolder(getParent(path))) {
-                return accessDeniedResponse();
+            if (body.getObjectType() == JobSchedulerObjectType.FOLDER) {
+                if (!this.folderPermissions.isPermittedForFolder(path)) {
+                    return accessDeniedResponse();
+                }
+
+            } else {
+
+                if (!this.folderPermissions.isPermittedForFolder(getParent(path))) {
+                    return accessDeniedResponse();
+                }
             }
 
             sosHibernateSession = Globals.createSosHibernateStatelessConnection(API_CALL);
 
-            DBLayerJoeObjects dbLayerJoeObjects = new DBLayerJoeObjects(sosHibernateSession);
-          // Satz in db schreiben.
+            DBLayerJoeObjects dbLayer = new DBLayerJoeObjects(sosHibernateSession);
+            FilterJoeObjects filter = new FilterJoeObjects();
+            filter.setConstraint(body);
+            DBItemJoeObject item = dbLayer.getJoeObject(filter);
+            final byte[] bytes = Globals.objectMapper.writeValueAsBytes(body.getConfiguration());
+            if (item != null) {
+                item.setOperation("store");
+                item.setAccount(getAccount());
+                item.setConfiguration(new String(bytes));
+                dbLayer.update(item);
 
-
-            /*DBItemJoeObject dbItemJoeObject = dbLayerJoeObjects.getJoeObject(filterJoeObjects);
-
-            JOCHotFolder jocHotFolder = new JOCHotFolder(this);
-            byte[] fileContent = jocHotFolder.getFile(path + Helper.getFileExtension(body.getObjectType()));
-
-            if (dbItemJoeObject == null || jocHotFolder.getLastModifiedDate().after(dbItemJoeObject.getModified())) {
-                body.setConfigurationDate(jocHotFolder.getLastModifiedDate());
             } else {
-                body.setConfigurationDate(dbItemJoeObject.getModified());
-                fileContent = dbItemJoeObject.getConfiguration().getBytes();
+                item = new DBItemJoeObject();
+                item.setId(null);
+                item.setAccount(getAccount());
+                item.setSchedulerId(body.getJobschedulerId());
+                item.setAuditLogId(null);
+                item.setConfiguration(new String(bytes));
+                item.setObjectType(body.getObjectType().value());
+                item.setOperation("store");
+                item.setPath(body.getPath());
+                dbLayer.save(item);
             }
 
-            body.setDeployed(false);
-
-            switch (body.getObjectType()) {
-            case JOB:
-                body.setConfiguration(Globals.xmlMapper.readValue(fileContent, ProcessClass.class));
-                break;
-            case ORDER:
-                body.setConfiguration(Globals.xmlMapper.readValue(fileContent, com.sos.joc.model.joe.order.Order.class));
-                break;
-            case JOBCHAIN:
-                body.setConfiguration(Globals.xmlMapper.readValue(fileContent, com.sos.joc.model.joe.jobchain.JobChain.class));
-                break;
-            case LOCK:
-                body.setConfiguration(Globals.xmlMapper.readValue(fileContent, com.sos.joc.model.joe.lock.Lock.class));
-                break;
-            case PROCESSCLASS:
-            case AGENTCLUSTER:
-                body.setConfiguration(Globals.xmlMapper.readValue(fileContent, com.sos.joc.model.joe.processclass.ProcessClass.class));
-                break;
-            case SCHEDULE:
-                body.setConfiguration(Globals.xmlMapper.readValue(fileContent, com.sos.joc.model.joe.schedule.Schedule.class));
-                break;
-            case MONITOR:
-                body.setConfiguration(Globals.xmlMapper.readValue(fileContent, ProcessClass.class));
-                break;
-
-            default:
-                break;
-            }
-*/
-            body.setDeliveryDate(Date.from(Instant.now()));
-            return JOCDefaultResponse.responseStatus200(body);
+            return JOCDefaultResponse.responseStatusJSOk(null);
 
         } catch (JocException e) {
             e.addErrorMetaInfo(getJocError());
