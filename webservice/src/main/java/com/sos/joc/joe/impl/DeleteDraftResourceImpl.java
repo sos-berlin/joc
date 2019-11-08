a751dd8a62e5fd10f706eddfac86421139221e1e
Uwe Risse <uwe.risse-git@sos-berlin.com>
2019-11-08 14:58:14 +0100

    JOC-730

--- webservice/src/main/java/com/sos/joc/joe/impl/DeleteDraftResourceImpl.java
+++ webservice/src/main/java/com/sos/joc/joe/impl/DeleteDraftResourceImpl.java
@@ -2,9 +2,7 @@ package com.sos.joc.joe.impl;
 
 import java.time.Instant;
 import java.util.Date;
-
 import javax.ws.rs.Path;
-
 import com.sos.auth.rest.permission.model.SOSPermissionJocCockpit;
 import com.sos.hibernate.classes.SOSHibernateSession;
 import com.sos.jobscheduler.model.event.CustomEvent;
@@ -13,11 +11,14 @@ import com.sos.joc.classes.JOEHelper;
 import com.sos.joc.classes.JOCDefaultResponse;
 import com.sos.joc.classes.JOCResourceImpl;
 import com.sos.joc.classes.calendar.SendCalendarEventsUtil;
+import com.sos.joc.db.joe.DBLayerJoeLocks;
 import com.sos.joc.db.joe.DBLayerJoeObjects;
 import com.sos.joc.db.joe.FilterJoeObjects;
 import com.sos.joc.exceptions.JocException;
+import com.sos.joc.exceptions.JoeFolderAlreadyLockedException;
 import com.sos.joc.joe.resource.IDeleteDraftResource;
 import com.sos.joc.model.joe.common.FilterDeploy;
+import com.sos.joc.model.joe.lock.LockInfo;
 
 @Path("joe")
 public class DeleteDraftResourceImpl extends JOCResourceImpl implements IDeleteDraftResource {
@@ -44,9 +45,17 @@ public class DeleteDraftResourceImpl extends JOCResourceImpl implements IDeleteD
             }
 
             sosHibernateSession = Globals.createSosHibernateStatelessConnection(API_CALL);
+            LockResourceImpl.unForcelock(new DBLayerJoeLocks(sosHibernateSession), body.getJobschedulerId(), body.getFolder(), getAccount());
+
             sosHibernateSession.setAutoCommit(false);
             DBLayerJoeObjects dbLayer = new DBLayerJoeObjects(sosHibernateSession);
             FilterJoeObjects filter = new FilterJoeObjects();
+            if (body.getAccount() == null || body.getAccount().isEmpty()) {
+                filter.setAccount(getAccount());
+            } else {
+                filter.setAccount(body.getAccount());
+            }
+
             if (body.getObjectName() != null) {
                 filter.setPath((body.getFolder() + "/").replaceAll("//+", "/") + body.getObjectName());
             } else {
@@ -72,6 +81,14 @@ public class DeleteDraftResourceImpl extends JOCResourceImpl implements IDeleteD
             }
 
             return JOCDefaultResponse.responseStatusJSOk(Date.from(Instant.now()));
+        } catch (JoeFolderAlreadyLockedException e) {
+            // e.addErrorMetaInfo(getJocError());
+            LockInfo entity = new LockInfo();
+            entity.setIsLocked(true);
+            entity.setLockedSince(e.getLockedSince());
+            entity.setLockedBy(e.getLockedBy());
+            entity.setDeliveryDate(Date.from(Instant.now()));
+            return JOCDefaultResponse.responseStatus434(entity);F
         } catch (JocException e) {
             e.addErrorMetaInfo(getJocError());
             return JOCDefaultResponse.responseStatusJSError(e);
