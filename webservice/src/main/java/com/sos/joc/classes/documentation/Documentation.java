95eee94e90090b41c2c0515fc7a90941c5261268
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2020-01-10 12:40:12 +0100

    JOC-795

--- webservice/src/main/java/com/sos/joc/classes/documentation/Documentation.java
+++ webservice/src/main/java/com/sos/joc/classes/documentation/Documentation.java
@@ -21,10 +21,50 @@ import com.sos.joc.model.common.JobSchedulerObjectType;
 
 public class Documentation {
 
+    public static String getDocumentationPath(String jobschedulerId, String jsObjectPath, JobSchedulerObjectType objType, String apiCall)
+            throws JocConfigurationException, DBConnectionRefusedException, DBInvalidDataException, SOSHibernateException, DBOpenSessionException {
+        SOSHibernateSession connection = null;
+        try {
+            connection = Globals.createSosHibernateStatelessConnection(apiCall);
+            return getDocumentationPath(connection, jobschedulerId, jsObjectPath, objType);
+        } finally {
+            Globals.disconnect(connection);
+        }
+    }
+
+    public static String getDocumentationPath(SOSHibernateSession connection, String jobschedulerId, String jsObjectPath,
+            JobSchedulerObjectType objType) throws JocConfigurationException, DBConnectionRefusedException, DBInvalidDataException,
+            SOSHibernateException, DBOpenSessionException {
+        DBItemDocumentationUsage dbDocUsage = getDocumentationUsage(connection, jobschedulerId, jsObjectPath, objType);
+        if (dbDocUsage == null) {
+            return null;
+        }
+        return dbDocUsage.getPath();
+    }
+
+    public static DBItemDocumentationUsage getDocumentationUsage(String jobschedulerId, String jsObjectPath, String docPath,
+            JobSchedulerObjectType objType, String apiCall) throws JocMissingRequiredParameterException, JocConfigurationException,
+            DBConnectionRefusedException, DBInvalidDataException, SOSHibernateException, DBOpenSessionException {
+        SOSHibernateSession connection = null;
+        try {
+            connection = Globals.createSosHibernateStatelessConnection(apiCall);
+            return getDocumentationUsage(connection, jobschedulerId, jsObjectPath, objType);
+        } finally {
+            Globals.disconnect(connection);
+        }
+    }
+
+    public static DBItemDocumentationUsage getDocumentationUsage(SOSHibernateSession connection, String jobschedulerId, String jsObjectPath,
+            JobSchedulerObjectType objType) throws JocConfigurationException, DBConnectionRefusedException, DBInvalidDataException,
+            SOSHibernateException, DBOpenSessionException {
+        DocumentationDBLayer dbLayer = new DocumentationDBLayer(connection);
+        String type = getObjectType(connection, jobschedulerId, jsObjectPath, objType);
+        return dbLayer.getDocumentationUsageForAssignment(jobschedulerId, jsObjectPath, type);
+    }
+
     public static void assignDocu(String jobschedulerId, String jsObjectPath, String docPath, JobSchedulerObjectType objType, String apiCall)
             throws JocMissingRequiredParameterException, JocConfigurationException, DBConnectionRefusedException, DBInvalidDataException,
-            SOSHibernateException, DBOpenSessionException
-             {
+            SOSHibernateException, DBOpenSessionException {
         SOSHibernateSession connection = null;
         try {
             connection = Globals.createSosHibernateStatelessConnection(apiCall);
@@ -33,22 +73,15 @@ public class Documentation {
             Globals.disconnect(connection);
         }
     }
-    
+
     public static void assignDocu(SOSHibernateSession connection, String jobschedulerId, String jsObjectPath, String docPath,
-            JobSchedulerObjectType objType) throws JocMissingRequiredParameterException, JocConfigurationException,
-            DBConnectionRefusedException, DBInvalidDataException, SOSHibernateException, DBOpenSessionException {
+            JobSchedulerObjectType objType) throws JocMissingRequiredParameterException, JocConfigurationException, DBConnectionRefusedException,
+            DBInvalidDataException, SOSHibernateException, DBOpenSessionException {
         if (docPath == null || docPath.isEmpty()) {
             throw new JocMissingRequiredParameterException(String.format("undefined '%1$s'", "documentation"));
         }
         DocumentationDBLayer dbLayer = new DocumentationDBLayer(connection);
-        String type = objType.name();
-        if (objType == JobSchedulerObjectType.WORKINGDAYSCALENDAR) {
-            CalendarsDBLayer calDbLayer = new CalendarsDBLayer(connection);
-            DBItemInventoryClusterCalendar dbCalendar = calDbLayer.getCalendar(jobschedulerId, jsObjectPath);
-            if (CalendarType.NON_WORKING_DAYS.name().equals(dbCalendar.getType())) {
-                type = JobSchedulerObjectType.NONWORKINGDAYSCALENDAR.name();
-            }
-        }
+        String type = getObjectType(connection, jobschedulerId, jsObjectPath, objType);
         DBItemDocumentationUsage dbDocUsage = dbLayer.getDocumentationUsageForAssignment(jobschedulerId, jsObjectPath, type);
         DBItemDocumentation dbDoc = dbLayer.getDocumentation(jobschedulerId, docPath);
         if (dbDocUsage != null) {
@@ -77,21 +110,27 @@ public class Documentation {
             Globals.disconnect(connection);
         }
     }
-    
+
     public static void unassignDocu(SOSHibernateSession connection, String jobschedulerId, String jsObjectPath, JobSchedulerObjectType objType)
             throws JocConfigurationException, DBConnectionRefusedException, DBInvalidDataException, SOSHibernateException, DBOpenSessionException {
         DocumentationDBLayer dbLayer = new DocumentationDBLayer(connection);
-        String type = objType.name();
-        if (objType == JobSchedulerObjectType.WORKINGDAYSCALENDAR) {
-            CalendarsDBLayer calDbLayer = new CalendarsDBLayer(connection);
-            DBItemInventoryClusterCalendar dbCalendar = calDbLayer.getCalendar(jobschedulerId, jsObjectPath);
-            if (CalendarType.NON_WORKING_DAYS.name().equals(dbCalendar.getType())) {
-                type = JobSchedulerObjectType.NONWORKINGDAYSCALENDAR.name();
-            }
-        }
+        String type = getObjectType(connection, jobschedulerId, jsObjectPath, objType);
         DBItemDocumentationUsage dbDocUsage = dbLayer.getDocumentationUsageForAssignment(jobschedulerId, jsObjectPath, type);
         if (dbDocUsage != null) {
             connection.delete(dbDocUsage);
         }
+    }
+
+    private static String getObjectType(SOSHibernateSession connection, String jobschedulerId, String jsObjectPath, JobSchedulerObjectType objType)
+            throws DBConnectionRefusedException, DBInvalidDataException {
+        String type = objType.value();
+        if (objType == JobSchedulerObjectType.WORKINGDAYSCALENDAR) {
+            CalendarsDBLayer calDbLayer = new CalendarsDBLayer(connection);
+            DBItemInventoryClusterCalendar dbCalendar = calDbLayer.getCalendar(jobschedulerId, jsObjectPath);
+            if (CalendarType.NON_WORKING_DAYS.value().equals(dbCalendar.getType())) {
+                type = JobSchedulerObjectType.NONWORKINGDAYSCALENDAR.value();
+            }
+        }
+        return type;
     }
 }
