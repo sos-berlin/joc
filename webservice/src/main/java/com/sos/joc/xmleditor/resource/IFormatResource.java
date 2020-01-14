48963b9aeda0450c5b6ffec328ff011a021bdacd
SOS-RE <robert.ehrlich@sos-berlin.com>
2020-01-14 19:23:14 +0100

    JOC-819 format web service

---
+++ webservice/src/main/java/com/sos/joc/xmleditor/resource/IFormatResource.java
@@ -0,0 +1,25 @@
+package com.sos.joc.xmleditor.resource;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.sos.joc.classes.JOCDefaultResponse;
+import com.sos.joc.classes.xmleditor.JocXmlEditor;
+import com.sos.joc.model.xmleditor.format.FormatConfiguration;
+
+public interface IFormatResource {
+
+    public static final String PATH = "format";
+    public static final String IMPL_PATH = JocXmlEditor.getResourceImplPath(PATH);
+
+    @POST
+    @Path(PATH)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces({ MediaType.APPLICATION_JSON })
+    public JOCDefaultResponse format(@HeaderParam("X-Access-Token") final String accessToken, FormatConfiguration in);
+
+}
