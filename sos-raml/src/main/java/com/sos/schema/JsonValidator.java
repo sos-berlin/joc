b74f9d49804d3b3b830ef9f33eb00aee925dec9b
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2020-01-27 11:46:19 +0100

    JOC-854

--- sos-raml/src/main/java/com/sos/schema/JsonValidator.java
+++ sos-raml/src/main/java/com/sos/schema/JsonValidator.java
@@ -31,9 +31,10 @@ public class JsonValidator {
     private static final Logger LOGGER = LoggerFactory.getLogger(JsonValidator.class);
     private static final ObjectMapper MAPPER = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     private static final SpecVersion.VersionFlag JSONDRAFT = SpecVersion.VersionFlag.V4;
-    //private static final Path RESOURCE_DIR = Paths.get("classpath:raml/schema", "schemas");
+    // private static final Path RESOURCE_DIR = Paths.get("classpath:raml/schema", "schemas");
     private static final List<NonValidationKeyword> NON_VALIDATION_KEYS = Arrays.asList(new NonValidationKeyword("javaType"),
-            new NonValidationKeyword("xmlElement"), new NonValidationKeyword("isXmlCData"), new NonValidationKeyword("isXmlAttribute"));
+            new NonValidationKeyword("javaInterfaces"), new NonValidationKeyword("extends"), new NonValidationKeyword("xmlElement"),
+            new NonValidationKeyword("isXmlCData"), new NonValidationKeyword("isXmlAttribute"));
     private static final JsonSchemaFactory FACTORY_V4 = JsonSchemaFactory.builder(JsonSchemaFactory.getInstance(JSONDRAFT)).addMetaSchema(
             JsonMetaSchema.builder(JsonMetaSchema.getV4().getUri(), JsonMetaSchema.getV4()).addKeywords(NON_VALIDATION_KEYS).build()).build();
 
@@ -45,7 +46,7 @@ public class JsonValidator {
             put("ConfigurationEdit", "joe/agentCluster/agentConfigurationEdit-schema.json");
             put("AuditLogFilter", "audit/auditLogFilter-schema.json");
             put("AuditParams", "audit/auditParams-schema.json");
-            
+
             put("CalendarDatesFilter", "calendar/calendarDatesFilter-schema.json");
             put("CalendarObjectFilter", "calendar/calendarObjectFilter-schema.json");
             put("CalendarRenameFilter", "calendar/calendarRenameFilter-schema.json");
@@ -53,22 +54,22 @@ public class JsonValidator {
             put("CalendarId", "calendar/calendarId-schema.json");
             put("CalendarImportFilter", "calendar/calendarImportFilter-schema.json");
             put("CalendarsFilter", "calendar/calendarsFilter-schema.json");
-            
+
             put("Configuration", "configuration/configuration-schema.json");
             put("ConfigurationsFilter", "configuration/configurationsFilter-schema.json");
             put("ConfigurationsDeleteFilter", "configuration/configurationsDeleteFilter-schema.json");
-            
+
             put("DocumentationShowFilter", "docu/documentationShow-schema.json");
             put("DocumentationFilter", "docu/documentationFilter-schema.json");
             put("DocumentationsFilter", "docu/documentationsFilter-schema.json");
             put("DocumentationImport", "docu/documentationImport-schema.json");
-            
+
             put("CheckEvent", "event/checkCustomEvent-schema.json");
             put("EventsFilter", "event/customEventsFilter-schema.json");
             put("RegisterEvent", "event/register-schema.json");
             put("ModifyEvent", "event/modifyCustomEvent-schema.json");
             put("EventIdsFilter", "event/customDeleteEventsFilter-schema.json");
-            
+
             put("JobFilter", "job/jobFilter-schema.json");
             put("JobConfigurationFilter", "job/jobConfigurationFilter-schema.json");
             put("JobDocuFilter", "job/jobDocuFilter-schema.json");
@@ -78,7 +79,7 @@ public class JsonValidator {
             put("StartJobs", "job/startJobs-schema.json");
             put("TaskFilter", "job/taskFilter-schema.json");
             put("ModifyTasks", "job/modifyTasks-schema.json");
-            
+
             put("JobChainFilter", "jobChain/jobChainFilter-schema.json");
             put("JobChainConfigurationFilter", "jobChain/jobChainConfigurationFilter-schema.json");
             put("JobChainDocuFilter", "jobChain/jobChainDocuFilter-schema.json");
@@ -86,40 +87,40 @@ public class JsonValidator {
             put("JobChainsFilter", "jobChain/jobChainsFilter-schema.json");
             put("ModifyJobChains", "jobChain/modifyJobChains-schema.json");
             put("ModifyJobChainNodes", "jobChain/modifyNode-schema.json");
-            
+
             put("LockConfigurationFilter", "lock/lockConfigurationFilter-schema.json");
             put("LockDocuFilter", "lock/lockDocuFilter-schema.json");
             put("LocksFilter", "lock/locksFilter-schema.json");
-            
+
             put("ProcessClassConfigurationFilter", "processClass/processClassConfigurationFilter-schema.json");
             put("ProcessClassDocuFilter", "processClass/processClassDocuFilter-schema.json");
             put("ProcessClassesFilter", "processClass/processClassesFilter-schema.json");
-            
-            put("ScheduleConfigurationFilter", "schedule/scheduleConfigurationFilter-schema.json"); //check
+
+            put("ScheduleConfigurationFilter", "schedule/scheduleConfigurationFilter-schema.json"); // check
             put("ScheduleDocuFilter", "schedule/scheduleDocuFilter-schema.json");
             put("ScheduleFilter", "schedule/scheduleFilter-schema.json");
             put("ModifyRunTime", "schedule/modifyRunTime-schema.json");
             put("SchedulesFilter", "schedule/schedulesFilter-schema.json");
-            
+
             put("OrderConfigurationFilter", "order/orderConfigurationFilter-schema.json");
             put("OrderDocuFilter", "order/orderDocuFilter-schema.json");
             put("OrderFilter", "order/orderFilter-schema.json");
             put("OrderHistoryFilter", "order/orderFilterWithHistoryId-schema.json");
             put("ModifyOrders", "order/modifyOrders-schema.json");
             put("OrdersFilter", "order/ordersFilter-schema.json");
-            
+
             put("FileFilter", "yade/fileFilter-schema.json");
             put("FilesFilter", "yade/filesFilter-schema.json");
             put("TransferFilter", "yade/transferFilter-schema.json");
             put("ModifyTransfer", "yade/modifyTransfer-schema.json");
             put("ModifyTransfers", "yade/modifyTransfers-schema.json");
-            
+
             put("HostPortParameter", "jobscheduler/urlParam-schema.json");
             put("TimeoutParameter", "jobscheduler/timeoutParam-schema.json");
             put("HostPortTimeOutParameter", "jobscheduler/urlTimeoutParam-schema.json");
             put("AgentClusterFilter", "jobscheduler/agentClusterFilter-schema.json");
             put("AgentFilter", "jobscheduler/agentFilter-schema.json");
-            
+
             put("JobSchedulerId", "common/jobSchedulerId-schema.json");
             put("JOClog", "joc/filename-schema.json");
             put("TreeFilter", "tree/treeFilter-schema.json");
@@ -127,7 +128,7 @@ public class JsonValidator {
             put("AgentsFilter", "report/agentsFilter-schema.json");
             put("RunTimePlanFilter", "plan/runTimePlanFilter-schema.json");
             put("PlanFilter", "plan/planFilter-schema.json");
-            
+
             put("ConditionEventsFilter", "jobstreams/conditionEventsFilter-schema.json");
             put("ConditionEvent", "jobstreams/conditionEvent-schema.json");
             put("InConditions", "jobstreams/inConditions-schema.json");
@@ -135,19 +136,19 @@ public class JsonValidator {
             put("JobStreams", "jobstreams/jobStreams-schema.json");
             put("JobStreamFilter", "jobstreams/jobstreamFilter-schema.json");
             put("ResetJobStream", "jobstreams/reset_jobstream-schema.json");
-            
+
             put("ApplyConfiguration", "xmleditor/apply/apply-configuration-schema.json");
-            put("AssignSchemaConfiguration", "xmleditor/schema/assign/assign-schema-configuration-schema.json");
+            put("AssignSchemaConfiguration", "xmleditor/schema/assign/schema-assign-configuration-schema.json");
             put("DeleteAll", "xmleditor/delete/all/delete-all-schema.json");
             put("DeleteDraft", "xmleditor/delete/delete-draft-schema.json");
             put("DeployConfiguration", "xmleditor/deploy/deploy-configuration-schema.json");
             put("ReadConfiguration", "xmleditor/read/read-configuration-schema.json");
-            put("ReassignSchemaConfiguration", "xmleditor/schema/reassign/reassign-schema-configuration-schema.json");
+            put("ReassignSchemaConfiguration", "xmleditor/schema/reassign/schema-reassign-configuration-schema.json");
             put("RenameConfiguration", "xmleditor/rename/rename-configuration-schema.json");
             put("StoreConfiguration", "xmleditor/store/store-configuration-schema.json");
             put("ValidateConfiguration", "xmleditor/validate/validate-configuration-schema.json");
             put("Xml2JsonConfiguration", "xmleditor/xml2json/xml2json-configuration-schema.json");
-            
+
             put("FilterDeploy", "joe/common/filter-deploy-schema.json");
             put("Filter", "joe/common/filter-schema.json");
             put("JSObjectEdit", "joe/common/jsObjectEdit-schema.json");
@@ -224,7 +225,7 @@ public class JsonValidator {
             validate(json, schemaUri, true);
         }
     }
-    
+
     // for testing
     protected static Map<String, String> getClassUriMap() {
         return CLASS_URI_MAPPING;
@@ -239,7 +240,7 @@ public class JsonValidator {
         }
         return FACTORY_V4.getSchema(schemaUri, config);
     }
-    
+
     private static String getSchemaPath(Class<?> clazz) {
         String schemaPath = CLASS_URI_MAPPING.get(clazz.getSimpleName());
         if (schemaPath == null) {
@@ -261,7 +262,7 @@ public class JsonValidator {
         } catch (JsonParseException e) {
             throw e;
         } catch (JsonSchemaException e) {
-            if (e.getCause() == null || e.getCause().getClass().isInstance(e) ) {
+            if (e.getCause() == null || e.getCause().getClass().isInstance(e)) {
                 throw new SOSJsonSchemaException(e.getMessage());
             }
             LOGGER.warn("JSON Validation impossible: " + e.toString());
